171ef742c4a35f7ee0164d5411ca4d1eb0555c6e
DESKTOP-VS26VVU\vijay <vijayaratnamythanushan000@gmail.com>
2023-09-11 16:21:40 +0530

    react-first-commit

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-dark text-light'>
+        <h6 className='text-center'>All rights reserved &copy: Thanushan Vijayaratnam</h6>
+    </div>
+  )
+}
