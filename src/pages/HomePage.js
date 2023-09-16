2478c658a6e577ddb498a0ddfc0c981bb485c40a
DESKTOP-VS26VVU\vijay <vijayaratnamythanushan000@gmail.com>
2023-09-16 14:53:45 +0530

    Filters-Update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,140 +1,175 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../components/Layout/Layout'
-import { Input, Modal, Form, Select, message, Table } from 'antd'
-import axios from 'axios';
-import Spinner from '../components/Spinner';
+import React, { useState, useEffect } from "react";
+import { Form, Input, message, Modal, Select, Table, DatePicker } from "antd";
+import Layout from "./../components/Layout/Layout";
+import axios from "axios";
+import Spinner from "./../components/Spinner";
+import moment from "moment";
+const { RangePicker } = DatePicker;
 
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
-  const [loading, setloading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [allTransection, setAllTransection] = useState([]);
-  const [frequency, setFrequency] = useState('7');
+  const [frequency, setFrequency] = useState("7");
+  const [selectedDate, setSelectedate] = useState([]);
+  const [type, setType] = useState("all");
 
-  //table data 
+  //table data
   const columns = [
     {
-      title:'Date',
-      dataIndex:'date'
+      title: "Date",
+      dataIndex: "date",
+      render: (text) => <span>{moment(text).format("YYYY-MM-DD")}</span>,
     },
     {
-      title:'Amount',
-      dataIndex:'amount'
+      title: "Amount",
+      dataIndex: "amount",
     },
     {
-      title:'Type',
-      dataIndex:'type'
+      title: "Type",
+      dataIndex: "type",
     },
     {
-      title:'Category',
-      dataIndex:'category'
-    }, {
-      title:'Refrence',
-      dataIndex:'refrence'
+      title: "Category",
+      dataIndex: "category",
     },
     {
-      title:'Actions',
+      title: "Refrence",
+      dataIndex: "refrence",
     },
-  ]
+    {
+      title: "Actions",
+    },
+  ];
 
-  //get all transections 
- 
-  //useeffect HOOK
+  //getall transactions
+
+  //useEffect Hook
   useEffect(() => {
-    const getAllTransections = async ()=>{
-      try{
-        const user = JSON.parse(localStorage.getItem('user'));
-        setloading(true);
-        const res = await axios.get('/transections/get-transection',
-           {userid: user._id,
-           frequency
+    const getAllTransactions = async () => {
+      try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        setLoading(true);
+        const res = await axios.post("/transections/get-transection", {
+          userid: user._id,
+          frequency,
+          selectedDate,
+          type,
         });
-        setloading(false);
+        setLoading(false);
         setAllTransection(res.data);
-        console.log(res.data)
-      }catch(error){
+        console.log(res.data);
+      } catch (error) {
         console.log(error);
-        message.error('Fetch Issue With Tranction')
+        message.error("Ftech Issue With Tranction");
       }
     };
-  
-    getAllTransections();
-  },[frequency]);
+    getAllTransactions();
+  }, [frequency, selectedDate, type]);
 
-  //form handiling 
+  // form handling
   const handleSubmit = async (values) => {
     try {
-      const user = JSON.parse(localStorage.getItem('user'))
-      setloading(true)
-      await axios.post('/transections/add-transection', { ...values, userid: user._id });
-      setloading(false);
-      message.success('Transaction added Successfully');
-      setShowModal(false)
+      const user = JSON.parse(localStorage.getItem("user"));
+      setLoading(true);
+      await axios.post("/transections/add-transection", {
+        ...values,
+        userid: user._id,
+      });
+      setLoading(false);
+      message.success("Transaction Added Successfully");
+      setShowModal(false);
     } catch (error) {
-      setloading(false);
-      message.error('Failed to add transection');
+      setLoading(false);
+      message.error("Faild to add transection");
     }
-  }
+  };
+
   return (
     <Layout>
-      {loading && <Spinner/>}
-      <div className='filters'>
+      {loading && <Spinner />}
+      <div className="filters">
         <div>
           <h6>Select Frequency</h6>
-          <Select value={frequency} onChange={(values)=>setFrequency(values)}>
-          <Select.Option value='7'>LAST 1 Week</Select.Option>
-          <Select.Option value='30'>LAST 1 Month</Select.Option>
-          <Select.Option value='365'>LAST 1 Year</Select.Option>
-          <Select.Option value='custom'>Custom</Select.Option>
+          <Select value={frequency} onChange={(values) => setFrequency(values)}>
+            <Select.Option value="7">LAST 1 Week</Select.Option>
+            <Select.Option value="30">LAST 1 Month</Select.Option>
+            <Select.Option value="365">LAST 1 year</Select.Option>
+            <Select.Option value="custom">custom</Select.Option>
           </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(values) => setSelectedate(values)}
+            />
+          )}
         </div>
         <div>
-          <button className='btn btn-primary' onClick={() => setShowModal(true)}>
+          <h6>Select Type</h6>
+          <Select value={type} onChange={(values) => setType(values)}>
+            <Select.Option value="all">ALL</Select.Option>
+            <Select.Option value="income">INCOME</Select.Option>
+            <Select.Option value="expense">EXPENSE</Select.Option>
+          </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(values) => setSelectedate(values)}
+            />
+          )}
+        </div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowModal(true)}
+          >
             Add New
           </button>
         </div>
       </div>
-      <div className='content'>
-      <Table columns={columns} dataSource={allTransection}/>
+      <div className="content">
+        <Table columns={columns} dataSource={allTransection} />
       </div>
-      <Modal title='Add Transection'
+      <Modal
+        title="Add Transection"
         open={showModal}
         onCancel={() => setShowModal(false)}
         footer={false}
       >
-        <Form layout='vertical' onFinish={handleSubmit}>
-          <Form.Item label='Amount' name='amount'>
-            <Input type='text' />
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item label="Amount" name="amount">
+            <Input type="text" />
           </Form.Item>
-          <Form.Item label='type' name='type'>
+          <Form.Item label="type" name="type">
             <Select>
-              <Select.Option value='income'>Income</Select.Option>
-              <Select.Option value='expense'>Expense</Select.Option>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item label='Category' name='category'>
+          <Form.Item label="Category" name="category">
             <Select>
-              <Select.Option value='salary'>Salary</Select.Option>
-              <Select.Option value='tip'>Tip</Select.Option>
-              <Select.Option value='project'>Project</Select.Option>
-              <Select.Option value='food'>Food</Select.Option>
-              <Select.Option value='movie'>Movie</Select.Option>
-              <Select.Option value='bills'>Bills</Select.Option>
-              <Select.Option value='medical'>Medical</Select.Option>
-              <Select.Option value='fee'>Fee</Select.Option>
-              <Select.Option value='tax'>Tax</Select.Option>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="tip">Tip</Select.Option>
+              <Select.Option value="project">Project</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="movie">Movie</Select.Option>
+              <Select.Option value="bills">Bills</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="fee">Fee</Select.Option>
+              <Select.Option value="tax">TAX</Select.Option>
             </Select>
           </Form.Item>
-          <Form.Item label='Date' name='date'>
-            <Input type='date' />
+          <Form.Item label="Date" name="date">
+            <Input type="date" />
           </Form.Item>
-          <Form.Item label='Refrence' name='refrence'>
-            <Input type='text' />
+          <Form.Item label="Refrence" name="refrence">
+            <Input type="text" />
           </Form.Item>
-          <Form.Item label='Description' name='description'>
-            <Input type='text' />
+          <Form.Item label="Description" name="description">
+            <Input type="text" />
           </Form.Item>
-          <div className='d-flex justify-content-end'>
-            <button className='btn btn-primary'>
+          <div className="d-flex justify-content-end">
+            <button type="submit" className="btn btn-primary">
               {" "}
               SAVE
             </button>
@@ -142,7 +177,7 @@ const HomePage = () => {
         </Form>
       </Modal>
     </Layout>
-  )
-}
+  );
+};
 
 export default HomePage;
